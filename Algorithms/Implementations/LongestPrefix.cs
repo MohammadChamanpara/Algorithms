76135eb0e228b83de8883fdf7531d508c524efeb
Mohammad Chamanpara <Mohammad.Chamanpara@akqa.com>
2020-04-21 15:15:44 +1000

    Longest Prefix

---
+++ Algorithms/Implementations/LongestPrefix.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class LongestPrefix
+{
+    public static void Run()
+    {
+        //var array = Tools.MakeArray(1, 2, 3);
+        //Console.WriteLine(new Solution().XXXX(123));
+    }
+
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+                return "";
+
+            if (string.IsNullOrEmpty(strs[0]))
+                return "";
+
+            StringBuilder prefix = new StringBuilder();
+            int size = strs[0].Length;
+
+            for (int i = 0; i < size; i++)
+            {
+                var c = strs[0][i];
+                bool equal = true;
+                foreach (var str in strs)
+                {
+                    if (str.Length <= i || str[i] != c)
+                    {
+                        equal = false;
+                        break;
+                    }
+                }
+                if (equal == false)
+                    break;
+                prefix.Append(c);
+            }
+            return prefix.ToString();
+        }
+    }
+}
