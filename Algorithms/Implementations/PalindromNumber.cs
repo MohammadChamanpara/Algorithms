ceba9046a29410979f3ce55de2a971539e5a83dc
Mohammad Chamanpara <Mohammad.Chamanpara@akqa.com>
2020-04-21 14:05:19 +1000

    Palindrome Number

---
+++ Algorithms/Implementations/PalindromNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class PalindromeNumber
+{
+    public static void Run()
+    {
+        //var array = Tools.MakeArray(1, 2, 3);
+        Console.WriteLine(new Solution().IsPalindrome(212321223));
+    }
+
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+
+            int ox = x;
+            int y = 0;
+            while (ox > 0)
+            {
+                y *= 10;
+                y += ox % 10;
+                ox /= 10;
+            }
+
+            return x == y;
+        }
+    }
+}
--- Algorithms/Implementations/_Empty.cs
+++ Algorithms/Implementations/_Empty.cs
@@ -7,7 +7,7 @@ class Empty
     public static void Run()
     {
         //var array = Tools.MakeArray(1, 2, 3);
-        //Console.WriteLine(new Solution.XXX());
+        //Console.WriteLine(new Solution().XXXX(123));
     }
 
     public class Solution
