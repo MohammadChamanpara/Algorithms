95dbaa4239fbecf72d7a45b2c50d3520d75f39e9
Mohammad Chamanpara <Mohammad.Chamanpara@akqa.com>
2020-04-21 10:54:48 +1000

    ReverseInt

---
+++ Algorithms/Implementations/ReverseInt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class ReverseInt
+{
+    public static void Run()
+    {
+
+        Console.WriteLine(new Solution().Reverse(123));
+    }
+
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            bool negative = false;
+            if (x < 0)
+            {
+                x = -x;
+                negative = true;
+            }
+            int y = 0;
+
+            try
+            {
+                while (x > 0)
+                {
+                    checked
+                    {
+                        y *= 10;
+                        y += x % 10;
+                    }
+                    x /= 10;
+                }
+            }
+            catch (OverflowException) {
+                return 0;
+            }
+
+            return negative ? -y : y;
+        }
+    }
+}
