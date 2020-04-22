9d816b13a9767eec8c89db4fbcf3b6783dc3bf37
Mohammad Chamanpara <Mohammad.Chamanpara@akqa.com>
2020-04-23 06:10:20 +1000

    Remove Duplicates from Sorted Array

---
+++ Algorithms/Implementations/RemoveDuplicatesfromSortedArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class RemoveDuplicatesfromSortedArray
+{
+    public static void Run()
+    {
+        //var array = Tools.MakeArray(1, 2, 3);
+        //Console.WriteLine(new Solution().XXXX(123));
+    }
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+            int cur = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[cur])
+                {
+                    nums[++cur] = nums[i];
+                }
+            }
+            return cur + 1;
+        }
+    }
+}
