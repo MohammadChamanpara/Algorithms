94aa4030030898bb8f2c62c428a733c74715d19a
Mohammad Chamanpara <Mohammad.Chamanpara@akqa.com>
2020-04-23 17:31:15 +1000

    Empty

---
+++ Algorithms/Implementations/_Empty - Copy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class c
+{
+    public static void Run()
+    {
+        //var array = Tools.MakeArray(1, 2, 3);
+        //Console.WriteLine(new Solution().XXXX(123));
+    }
+
+}
