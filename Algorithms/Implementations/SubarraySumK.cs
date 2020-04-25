ff45fb36743ba5e009b86c2cc878145c001e80f7
Mohammad Chamanpara <Mohammad.Chamanpara@gmail.com>
2020-04-26 06:58:39 +1000

    SubArray

---
+++ Algorithms/Implementations/SubarraySumK.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SubarraySumK
+{
+    public static void Run()
+    {
+        var array = Tools.MakeArray(1, 2, -1, 8, 6, -4, -10, -1, -5);
+        Console.WriteLine(new Solution().SubarraySum(array, 6));
+    }
+    public class Solution
+    {
+        public int SubarraySum(int[] nums, int k)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            int sum = nums[0] == k ? 1 : 0;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+                if (nums[i] == k)
+                    sum++;
+            }
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                for (int j = i; j < nums.Length; j++)
+                {
+                    nums[j] -= nums[i - 1];
+                    if (nums[j] == k)
+                        sum++;
+                }
+            }
+            return sum;
+        }
+    }
+}
